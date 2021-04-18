4d105278eea5ef2233ec0caf3904f30054f94a89
DmytroHoretskyi <serwserw16@gmail.com>
2021-04-18 15:43:40 +0300

    fixed some mistakes

--- Cycle.java
+++ Cycle.java
@@ -1,4 +1,4 @@
-public class Cycle{
+ public class Cycle{
     private int numbersOfGears = 0;
     private double weight = 0;
     private double maxWeight = 0;
@@ -9,92 +9,32 @@ public class Cycle{
 
     private static int yearOfProduction;
 
-    public Cycle(int numbersOfGears, double weight, double maxWeight, String producer, String color, String maxSpeed, String type) {
-        this.numbersOfGears = numbersOfGears;
-        this.weight = weight;
-        this.maxWeight = maxWeight;
-        this.producer = producer;
-        this.color = color;
-        this.maxSpeed = maxSpeed;
-        this.type = type;
-    }
+    protected int wheelWidth = 0;
+    protected boolean isBasket = false;
 
-    public Cycle(int numbersOfGears, double weight, double maxWeight, String producer, String color, String maxSpeed) {
-        this.numbersOfGears = numbersOfGears;
-        this.weight = weight;
-        this.maxWeight = maxWeight;
-        this.producer = producer;
-        this.color = color;
-        this.maxSpeed = maxSpeed;
+    public Cycle(){
+        this(0, 0, 0, null, null);
     }
 
     public Cycle(int numbersOfGears, double weight, double maxWeight, String producer, String color) {
+        this(numbersOfGears, weight, maxWeight, producer, color, null);
+    }
+
+    public Cycle(int numbersOfGears, double weight, double maxWeight, String producer, String color, String maxSpeed) {
+        this(numbersOfGears, weight, maxWeight, producer, color, maxSpeed, null, 0, false);
+    }
+
+    public Cycle(int numbersOfGears, double weight, double maxWeight, String producer, String color, String maxSpeed, String type, int wheelWidth, boolean isBasket) {
         this.numbersOfGears = numbersOfGears;
         this.weight = weight;
         this.maxWeight = maxWeight;
         this.producer = producer;
         this.color = color;
-    }
-
-    public static int getYearOfProduction() {
-        return yearOfProduction;
-    }
-    public int getNumbersOfGears() {
-        return numbersOfGears;
-    }
-
-    public void setNumbersOfGears(int numbersOfGears) {
-        this.numbersOfGears = numbersOfGears;
-    }
-
-    public double getWeight() {
-        return weight;
-    }
-
-    public void setWeight(double weight) {
-        this.weight = weight;
-    }
-
-    public double getMaxWeight() {
-        return maxWeight;
-    }
-
-    public void setMaxWeight(double maxWeight) {
-        this.maxWeight = maxWeight;
-    }
-
-    public String getProducer() {
-        return producer;
-    }
-
-    public void setProducer(String producer) {
-        this.producer = producer;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getMaxSpeed() {
-        return maxSpeed;
-    }
-
-    public void setMaxSpeed(String maxSpeed) {
         this.maxSpeed = maxSpeed;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
         this.type = type;
+        this.wheelWidth = wheelWidth;
+        this.isBasket = isBasket;
     }
-
 
     @Override
     public String toString() {
@@ -109,5 +49,95 @@ public class Cycle{
                 '}';
     }
 
+     public int getNumbersOfGears() {
+         return numbersOfGears;
+     }
 
-}
+     public void setNumbersOfGears(int numbersOfGears) {
+         this.numbersOfGears = numbersOfGears;
+     }
+
+     public double getWeight() {
+         return weight;
+     }
+
+     public void setWeight(double weight) {
+         this.weight = weight;
+     }
+
+     public double getMaxWeight() {
+         return maxWeight;
+     }
+
+     public void setMaxWeight(double maxWeight) {
+         this.maxWeight = maxWeight;
+     }
+
+     public String getProducer() {
+         return producer;
+     }
+
+     public void setProducer(String producer) {
+         this.producer = producer;
+     }
+
+     public String getColor() {
+         return color;
+     }
+
+     public void setColor(String color) {
+         this.color = color;
+     }
+
+     public String getMaxSpeed() {
+         return maxSpeed;
+     }
+
+     public void setMaxSpeed(String maxSpeed) {
+         this.maxSpeed = maxSpeed;
+     }
+
+     public String getType() {
+         return type;
+     }
+
+     public void setType(String type) {
+         this.type = type;
+     }
+
+     public static int getYearOfProduction() {
+         return yearOfProduction;
+     }
+
+     public static void setYearOfProduction(int yearOfProduction) {
+         Cycle.yearOfProduction = yearOfProduction;
+     }
+
+     public int getWheelWidth() {
+         return wheelWidth;
+     }
+
+     public void setWheelWidth(int wheelWidth) {
+         this.wheelWidth = wheelWidth;
+     }
+
+     public boolean isBasket() {
+         return isBasket;
+     }
+
+     public void setBasket(boolean basket) {
+         isBasket = basket;
+     }
+
+     public void resetValues(int numbersOfGears, double weight, double maxWeight, String producer, String color, String maxSpeed, String type, int wheelWidth, boolean isBasket) {
+         this.numbersOfGears = numbersOfGears;
+         this.weight = weight;
+         this.maxWeight = maxWeight;
+         this.producer = producer;
+         this.color = color;
+         this.maxSpeed = maxSpeed;
+         this.type = type;
+         this.wheelWidth = wheelWidth;
+         this.isBasket = isBasket;
+     }
+ }
--- Main.java
+++ Main.java
@@ -1,13 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        Cycle mountainCycle = new Cycle(2, 50.5, 80,
-                "Ukraine", "Blue", "450mph", "Mountain");
+        Cycle mountainCycle = new Cycle();
 
         Cycle importCycle = new Cycle(5, 20, 50, "German", "Black");
-        Cycle simpleCycle = new Cycle(2, 30, 80, "Europe", "Pink");
+        Cycle simpleCycle = new Cycle(2, 30, 80, "Europe", "Pink", "45");
+        Cycle ukrainianCycle = new Cycle(1, 50,150,"Ukraina", "blue", "15", "sport", 24, true);
 
         System.out.println(mountainCycle);
         System.out.println(importCycle);
         System.out.println(simpleCycle);
+        System.out.println(ukrainianCycle);
     }
 }
